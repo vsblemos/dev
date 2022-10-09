582db515c480e31cc36e24d3c8dca6d78da6ea24
Victor Serra B Lemos <91221341+vsblemos@users.noreply.github.com>
2022-10-09 17:06:57 -0300

    create ranking and progress bar

--- ead/src/App.js
+++ ead/src/App.js
@@ -5,6 +5,8 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Course from "./components/Course";
 import Certificate from "./components/Certificate";
+import Ranking from "./components/Ranking";
+import Progress from "./components/Progress";
 
 
 const App = () =>{
@@ -35,11 +37,12 @@ const App = () =>{
 
       <div className="col-3" id="ranking">
       <div id="main">
-        <div id="class-container">
-          
-          <div>
-          
-          </div>
+        <div id="ranking">
+          <Ranking></Ranking>
+        </div>
+        <div id="progress">
+          <Progress></Progress>
+
         </div>
 
 
---
+++ ead/src/components/Progress.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "../App.css"
+
+
+const Progress = () =>{
+    return(<>
+    <div id="ranking-container">
+        
+    </div>
+    </>)
+}
+
+export default Progress
