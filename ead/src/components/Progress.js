2b1c8ff918ad78f8a0c3cb268cef7b6a72087d15
Victor Serra B Lemos <91221341+vsblemos@users.noreply.github.com>
2022-10-09 18:01:44 -0300

    HTML

--- ead/src/components/Progress.js
+++ ead/src/components/Progress.js
@@ -6,8 +6,14 @@ import "../App.css"
 
 const Progress = () =>{
     return(<>
-    <div id="ranking-container">
-        
+    <div id="progress-container">
+        <h2>Your Progress</h2>
+        <div className="circular-progress-container d-flex flex-column justify-content-center align-items-center">
+            <div className="circular-progress d-flex justify-content-center align-items-center ">
+                 <span className="progress-value ">60%</span>
+            </div>
+        </div>
+        <h3>Keep Going!</h3>
     </div>
     </>)
 }
